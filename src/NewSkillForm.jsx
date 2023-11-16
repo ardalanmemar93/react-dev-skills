c889fff6cfea69470475225a78864aa8eaaf4a1c
ardalanmemar93 <ardalanmemar@gmail.com>
2023-11-16 15:02:25 -0800

    working on form submition

--- src/NewSkillForm.jsx
+++ src/NewSkillForm.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
 import "./NewSkillForm.css";
 
+
 export default function NewSkillForm() {
+  const [formData, setFormData] = useState({
+    skill: "",
+    level: "noob",
+  });
+
+  function handleChange(evt) {
+    const newFormData = {...formData, [ evt.target.name] : evt.target.value}
+    setFormData(newFormData)
+  }
+
   return (
     <div className="FormWrapper">
     <form className="NewSkillForm">
       
-      <label htmlFor="skill">Skill: </label>
-      <input type="text" id="skill" name="skill" placeholder="Enter your skill" />
+      <label htmlFor="skill" className="skill">Skill: </label>
+      <input value={formData.skill} onChange={handleChange}
+      type="text" id="skill" name="skill" placeholder="Enter your skill" />
       
-      <label htmlFor="level">Level: </label>
-      <select id="level" name="level">
+      <label htmlFor="level" className="level">Level: </label>
+      <select value={formData.level} onChange={handleChange}
+      id="level" name="level">
         <option value="noob">Noob</option>
         <option value="intermediate">Intermediate</option>
         <option value="advanced">Advanced</option>
@@ -23,6 +37,7 @@ export default function NewSkillForm() {
       <button type="submit" className="AddSkill">ADD SKILL</button>
       
     </form>
+    <h2>{formData.skill} is {formData.level}</h2>
     </div>
   );
 }
